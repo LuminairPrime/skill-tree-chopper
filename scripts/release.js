be17bdc2758624ec298e2528399bbfa3b745b967
LuminairPrime <luminair@gmail.com>
2026-05-18 01:24:12 -0300

    chore: format release.js

--- scripts/release.js
+++ scripts/release.js
@@ -5,7 +5,9 @@ const releaseType = process.argv[2] || 'patch';
 
 if (cp.execSync('git status --porcelain').toString().trim()) {
   const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-  console.log(`\x1b[33m⚠️ Warning: cannot release until changes are committed. Try: git commit -am "before release of ${pkg.version}"\x1b[0m`);
+  console.log(
+    `\x1b[33m⚠️ Warning: cannot release until changes are committed. Try: git commit -am "before release of ${pkg.version}"\x1b[0m`,
+  );
   process.exit(0);
 }
 
