dfb02a96d754261be314a67d7e2cd26771f25981
LuminairPrime <luminair@gmail.com>
2026-05-18 01:14:09 -0300

    chore: improve check-clean task output and quietness

--- scripts/check-clean.js
+++ scripts/check-clean.js
@@ -1,7 +1,11 @@
 const cp = require('child_process');
+const fs = require('fs');
+
 if (cp.execSync('git status --porcelain').toString().trim()) {
-  console.error(
-    '? ERROR: Git working directory is not clean. Please commit or stash your changes before publishing.',
-  );
+  const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+  console.log(`\x1b[33m⚠️  Warning: Git working directory is not clean.\x1b[0m`);
+  console.log(`Please commit or stash your changes before publishing.`);
+  console.log(`\nSuggested commit command:`);
+  console.log(`\x1b[36mgit commit -am "chore: prepare for release ${pkg.version}"\x1b[0m\n`);
   process.exit(1);
 }
